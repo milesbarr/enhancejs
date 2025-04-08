f9eb4e336ce476c0395a3bf7400e660f3fcda8e3
Miles Barr <1018475+milesbarr@users.noreply.github.com>
2025-04-08 08:07:55 -0700

    Update

--- html-enhancer.js
+++ html-enhancer.js
@@ -88,7 +88,7 @@ document.addEventListener("input", event => {
 document.addEventListener("click", event => {
   const link = event.target.closest("a[href^='#']");
   if (!link) return;
-  const target = document.querySelector(link.getAttribute("href"));
+  const target = document.querySelector(link.href);
   if (!target) return;
   event.preventDefault();
   target.scrollIntoView({ behavior: "smooth" });
